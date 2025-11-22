be3901cd7890dc82fff2c7662282ce83e5c9f28b
kushvlshresthv <kushvlshresthv@gmail.com>
2025-11-22 18:59:16 +0545

    fixed bug for /updateCommitteeDetails where new member addition was not being persisted due to CASCADE MERGE not being set

--- backend/src/main/java/com/sep/mmms_backend/entity/Committee.java
+++ backend/src/main/java/com/sep/mmms_backend/entity/Committee.java
@@ -82,7 +82,7 @@ public class Committee {
     @OneToMany(mappedBy="committee", cascade = CascadeType.REMOVE)
     private List<Meeting> meetings = new ArrayList<>();
 
-    @OneToMany(mappedBy = "committee", cascade = {CascadeType.PERSIST, CascadeType.REMOVE}, orphanRemoval = true)
+    @OneToMany(mappedBy = "committee", cascade = {CascadeType.PERSIST, CascadeType.REMOVE, CascadeType.MERGE}, orphanRemoval = true)
     private List<CommitteeMembership> memberships = new ArrayList<>();
 
     /**
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -43,12 +43,12 @@ INSERT INTO committees (
 )
 VALUES
     ('Academic Committee',                 'Oversee academic policies and curriculum development', 1, CURDATE(), 'username', CURDATE(), UUID(), 'ACTIVE', 10, 'NEPALI', 1),
-    ('Events Committee',                   'Plan and organize all institutional events and seminars', 1, CURDATE(), 'username', CURDATE(), UUID(),'ACTIVE', 10, 'ENGLISH', 9),
+    ('Events Committee',                   'Plan and organize all institutional events and seminars', 1, CURDATE(), 'username', CURDATE(), UUID(),'ACTIVE', 10, 'ENGLISH', 1),
     ('Research and Development Committee', 'Promote research and innovation', 1, CURDATE(), 'username', CURDATE(), UUID(), 'ACTIVE', 10, 'NEPALI', 1),
     ('Disciplinary Committee',             'Handle student and staff disciplinary issues', 1, CURDATE(), 'username', CURDATE(), UUID(), 'ACTIVE', 10, 'NEPALI', 1),
     ('Student Welfare Committee',          'Addresse student concerns and well-being', 1, CURDATE(), 'username', CURDATE(), UUID(),'ACTIVE', 10, 'NEPALI', 1),
     ('IT and Infrastructure Committee',    'Manage IT resources and campus infrastructure', 2, CURDATE(), 'username', CURDATE(), UUID(), 'ACTIVE', 10, 'NEPALI', 1),
-    ('इ.अ.स. BE/BArch केन्द्रीकृत भर्ना २०८२ अनुगमन',    'त्रि.वि. इ.अ.स. अन्तर्गत आंगिक क्याम्पस तथा सम्बन्धन प्राप्त कलेजहरुमा शैक्षिक वर्ष २०८२/०८३ मा संचालन हुने स्नातक (BE/BArch) तहका विभिन्न कार्यक्रमहरुमा केन्द्रीकृत भर्ना अनुगमन', 1, CURDATE(), 'username', CURDATE(), UUID(), 'ACTIVE', 10, 'NEPALI', 1);
+    ('इ.अ.स. BE/BArch केन्द्रीकृत भर्ना २०८२ अनुगमन',    'त्रि.वि. इ.अ.स. अन्तर्गत आंगिक क्याम्पस तथा सम्बन्धन प्राप्त कलेजहरुमा शैक्षिक वर्ष २०८२/०८३ मा संचालन हुने स्नातक (BE/BArch) तहका विभिन्न कार्यक्रमहरुमा केन्द्रीकृत भर्ना अनुगमन', 1, CURDATE(), 'username', CURDATE(), UUID(), 'ACTIVE', 1, 'NEPALI', 1);
 
 -- Insert Meetings
 INSERT INTO meetings (
@@ -109,7 +109,6 @@ VALUES
 INSERT INTO committee_memberships (committee_id, member_id, role, uuid)
 VALUES
     -- Committee 1 (8 members)
-    (1, 1, 'सदस्य सचिव', UUID()),
     (1, 2, 'सदस्य', UUID()),
     (1, 3, 'सदस्य', UUID()),
     (1, 4, 'सदस्य', UUID()),
